de9508d145398400a602157270fd91ee95eb94a1
Sebastian Kranz <sebkranz@gmail.com>
2023-03-08 13:33:42 +0100

    Update embedding.R

--- R/embedding.R
+++ R/embedding.R
@@ -3,9 +3,21 @@ example_embedding = function() {
   set_openai_key(path="C:/libraries/gpt4r/openai.key")
 
   input = "Hi, I am a text that wants to get an embedding."
-
   resp = gpt_embedding(input)
-  emb = resp$embedding
+  emb1 = resp$embedding
+
+  input = "Hi, I am another text that would like to have an embedding."
+  resp = gpt_embedding(input)
+  emb2 = resp$embedding
+
+  input = "This is quite different"
+  resp = gpt_embedding(input)
+  emb3 = resp$embedding
+
+  emb.mat = rbind(emb1,emb2, emb3)
+
+  sum(emb^2)
+  sd(emb)
 
 }
 
@@ -17,6 +29,10 @@ gpt_embedding = function(input){
 
   resp = httr::POST(url = "https://api.openai.com/v1/embeddings", body = parameter_list, httr::add_headers(Authorization = paste("Bearer", openai_key)), encode = "json")
 
+  if (!isTRUE(resp$status_code == 200)) {
+    resp$error = content(resp)$error
+    cat("\n",resp$error$message)
+  }
   restore.point("gpt_embedding2")
   output_base = httr::content(resp)
 
